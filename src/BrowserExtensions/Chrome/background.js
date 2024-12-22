ab17bd3d21b1b66defb63ac0e01637588b128495
elzik <23397871+elzik@users.noreply.github.com>
2024-12-22 22:05:58 +0000

    Add extensions notifications

--- src/BrowserExtensions/Chrome/background.js
+++ src/BrowserExtensions/Chrome/background.js
@@ -9,8 +9,24 @@ chrome.action.onClicked.addListener((tab) => {
                 body: JSON.stringify({ url: tab.url })
             })
                 .then(response => response.json())
-                .then(data => console.log('Success:', data))
-                .catch((error) => console.error('Error:', error));
+                .then(data => {
+                    console.log('Success:', data);
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: 'Images/breef-48.png',
+                        title: 'Request Successful',
+                        message: 'The URL was sent successfully.'
+                    });
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: 'Images/breef-48.png',
+                        title: 'Request Failed',
+                        message: 'There was an error sending the URL.'
+                    });
+                });
         });
     }
 });
