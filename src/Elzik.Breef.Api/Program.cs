ce16978c8e035ad012b2b10410ebed0a1b07a704
elzik <23397871+elzik@users.noreply.github.com>
2025-01-01 18:48:21 +0000

    Add functional tests

--- src/Elzik.Breef.Api/Program.cs
+++ src/Elzik.Breef.Api/Program.cs
@@ -1,57 +1,69 @@
 using AspNetCore.Authentication.ApiKey;
-using Elzik.Breef.Api;
 using System.Diagnostics;
 
-var builder = WebApplication.CreateSlimBuilder(args);
+namespace Elzik.Breef.Api;
 
-builder.Services.ConfigureHttpJsonOptions(options =>
+public class Program
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder =>
+    protected Program()
     {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
-});
-
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-builder.Services.AddAuthentication(ApiKeyDefaults.AuthenticationScheme)
-    .AddApiKeyInHeader<EnvironmentApiKeyProvider>(options =>
-    {
-        options.KeyName = "BREEF-API-KEY";
-        options.Realm = "BreefAPI";
-    });
-builder.Services.AddAuthorization();
-
-var app = builder.Build();
-app.UseCors();
-app.UseAuthentication();
-app.UseAuthorization();
-app.Use(async (context, next) =>
-{
-    if (context.User.Identity != null && !context.User.Identity.IsAuthenticated)
-    {
-        context.Response.StatusCode = 401;
-        await context.Response.WriteAsync("Unauthorized");
-        return;
+        // This satisfies SonarQube's S1118: Utility classes should not have public constructors
     }
-    await next();
-});
 
-var breefs = app.MapGroup("/breefs");
-breefs.MapPost("/", async (Breef breef) =>
-{
-    Debug.WriteLine(DateTime.Now.TimeOfDay.TotalNanoseconds + ": " + breef.Url);
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateSlimBuilder(args);
 
-    return Results.Created(breef.Url, breef);
-});
+        builder.Services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+        });
 
-await app.RunAsync();
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+        });
+
+        builder.Services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+        });
+        builder.Services.AddAuthentication(ApiKeyDefaults.AuthenticationScheme)
+            .AddApiKeyInHeader<EnvironmentApiKeyProvider>(options =>
+            {
+                options.KeyName = "BREEF-API-KEY";
+                options.Realm = "BreefAPI";
+            });
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+        app.UseCors();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.Use(async (context, next) =>
+        {
+            if (context.User.Identity != null && !context.User.Identity.IsAuthenticated)
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorised");
+                return;
+            }
+            await next();
+        });
+
+        var breefs = app.MapGroup("/breefs");
+        breefs.MapPost("/", async (Breef breef) =>
+        {
+            Debug.WriteLine(DateTime.Now.TimeOfDay.TotalNanoseconds + ": " + breef.Url);
+
+            return Results.Created(breef.Url, breef);
+        });
+
+        await app.RunAsync();
+    }
+}
