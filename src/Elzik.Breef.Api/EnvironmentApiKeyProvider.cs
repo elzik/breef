4a263bc3758d5470ceada74a34016133976c7d45
elzik <23397871+elzik@users.noreply.github.com>
2025-01-06 17:44:08 +0000

    Fix CS8625

--- src/Elzik.Breef.Api/EnvironmentApiKeyProvider.cs
+++ src/Elzik.Breef.Api/EnvironmentApiKeyProvider.cs
@@ -4,18 +4,18 @@ namespace Elzik.Breef.Api;
 
 public class EnvironmentApiKeyProvider : IApiKeyProvider
 {
-    private const string ApiKeyEnvirnomentVariableName = "BREEF_API_KEY";
-    private readonly string ApiKey = Environment.GetEnvironmentVariable(ApiKeyEnvirnomentVariableName) 
+    private const string ApiKeyEnvironmentVariableName = "BREEF_API_KEY";
+    private readonly string ApiKey = Environment.GetEnvironmentVariable(ApiKeyEnvironmentVariableName)
         ?? throw new InvalidOperationException(
-            $"An environment variable named {ApiKeyEnvirnomentVariableName} must be provided.");
+            $"An environment variable named {ApiKeyEnvironmentVariableName} must be provided.");
 
-    public Task<IApiKey> ProvideAsync(string key)
+    public Task<IApiKey?> ProvideAsync(string key)
     {
         if (key == ApiKey)
         {
-            return Task.FromResult<IApiKey>(new ApiKey(key));
+            return Task.FromResult<IApiKey?>(new ApiKey(key));
         }
 
-        return Task.FromResult<IApiKey>(null);
+        return Task.FromResult<IApiKey?>(null);
     }
 }
