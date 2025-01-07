4c12af360cfb9596f46795c3eca2bf5e26fc0cbd
elzik <23397871+elzik@users.noreply.github.com>
2025-01-07 22:15:56 +0000

    Fix path in Docker build

--- build/api/build-docker.ps1
+++ build/api/build-docker.ps1
@@ -2,5 +2,5 @@ $repoRoot =  Resolve-Path "$PSScriptRoot/../.."
 
 docker build `
     -t ghcr.io/elzik/elzik-breef-api:latest `
-    -f src/Elzik.Breef.Api/Dockerfile `
+        -f "$repoRoot/src/Elzik.Breef.Api/Dockerfile" `
     "$repoRoot/src"
