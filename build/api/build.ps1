cdc6f13cdf70375fc833b5c06d3144cc44311934
elzik <23397871+elzik@users.noreply.github.com>
2025-01-15 22:38:04 +0000

    Do not catch errors

--- build/api/build.ps1
+++ build/api/build.ps1
@@ -1,10 +1,5 @@
 $repoRoot = "$PSScriptRoot/../.."
 
-try {
-    dotnet build $repoRoot `
-        --configuration Release `
-        --no-restore
-} catch {
-    $global:LASTEXITCODE = 1
-    Write-Error $_
-}
+dotnet build $repoRoot `
+    --configuration Release `
+    --no-restore
