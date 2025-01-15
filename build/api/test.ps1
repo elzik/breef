6c6c3352829abfa8e1c6b66583fc7065de36c2b0
elzik <23397871+elzik@users.noreply.github.com>
2025-01-15 22:11:21 +0000

    Improve error reporting in test script

--- build/api/test.ps1
+++ build/api/test.ps1
@@ -1,18 +1,28 @@
-$repoRoot =  Resolve-Path "$PSScriptRoot/../.."
+try
+{
+	$repoRoot =  Resolve-Path "$PSScriptRoot/../.."
 
-dotnet test $repoRoot `
-    --configuration Release `
-    --no-build `
-    --verbosity normal `
-    --logger 'trx;LogFileName=test-results.trx' `
-    -p:CollectCoverage=true `
-    -p:CoverletOutput=TestResults/coverage.opencover.xml `
-    -p:CoverletOutputFormat=opencover
-dotnet tool update `
-    --global dotnet-reportgenerator-globaltool `
-    --version 5.*
-reportgenerator `
-    "-reports:$repoRoot/tests/Elzik.Breef.Api.Tests.Functional/TestResults/coverage.opencover.xml" `
-    "-targetdir:$repoRoot/tests/TestResults" `
-    "-reporttypes:Badges;Cobertura" `
-    "-filefilters:-*.g.cs" # https://github.com/danielpalme/ReportGenerator/issues/457
+    dotnet test $repoRoot `
+        --configuration Release `
+        --no-build `
+        --verbosity normal `
+        --logger 'trx;LogFileName=test-results.trx' `
+        -p:CollectCoverage=true `
+        -p:CoverletOutput=TestResults/coverage.opencover.xml `
+        -p:CoverletOutputFormat=opencover
+    dotnet tool update `
+        --global dotnet-reportgenerator-globaltool `
+        --version 5.*
+    reportgenerator `
+        "-reports:$repoRoot/tests/Elzik.Breef.Api.Tests.Functional/TestResults/coverage.opencover.xml" `
+        "-targetdir:$repoRoot/tests/TestResults" `
+        "-reporttypes:Badges;Cobertura" `
+        "-filefilters:-*.g.cs" # https://github.com/danielpalme/ReportGenerator/issues/457
+}
+catch
+{
+    foreach ($err in $Error)
+    {
+        Write-Error $err
+    }
+}
