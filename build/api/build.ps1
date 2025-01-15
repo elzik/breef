60c998cb24242bc24aebe16905bd0f23934c1c0c
elzik <23397871+elzik@users.noreply.github.com>
2025-01-15 22:19:23 +0000

    Ensure GitHub step fails when script errors

--- build/api/build.ps1
+++ build/api/build.ps1
@@ -1,5 +1,10 @@
 $repoRoot = "$PSScriptRoot/../.."
 
-dotnet build $repoRoot `
-    --configuration Release `
-    --no-restore
+try {
+    dotnet build $repoRoot `
+        --configuration Release `
+        --no-restore -ErrorAction Stop
+} catch {
+    Write-Error "Build failed with error: $_"
+    throw
+}
--- build/api/test.ps1
+++ build/api/test.ps1
@@ -1,28 +1,18 @@
-try
-{
-	$repoRoot =  Resolve-Path "$PSScriptRoot/../.."
+$repoRoot =  Resolve-Path "$PSScriptRoot/../.."
 
-    dotnet test $repoRoot `
-        --configuration Release `
-        --no-build `
-        --verbosity normal `
-        --logger 'trx;LogFileName=test-results.trx' `
-        -p:CollectCoverage=true `
-        -p:CoverletOutput=TestResults/coverage.opencover.xml `
-        -p:CoverletOutputFormat=opencover
-    dotnet tool update `
-        --global dotnet-reportgenerator-globaltool `
-        --version 5.*
-    reportgenerator `
-        "-reports:$repoRoot/tests/Elzik.Breef.Api.Tests.Functional/TestResults/coverage.opencover.xml" `
-        "-targetdir:$repoRoot/tests/TestResults" `
-        "-reporttypes:Badges;Cobertura" `
-        "-filefilters:-*.g.cs" # https://github.com/danielpalme/ReportGenerator/issues/457
-}
-catch
-{
-    foreach ($err in $Error)
-    {
-        Write-Error $err
-    }
-}
+dotnet test $repoRoot `
+    --configuration Release `
+    --no-build `
+    --verbosity normal `
+    --logger 'trx;LogFileName=test-results.trx' `
+    -p:CollectCoverage=true `
+    -p:CoverletOutput=TestResults/coverage.opencover.xml `
+    -p:CoverletOutputFormat=opencover
+dotnet tool update `
+    --global dotnet-reportgenerator-globaltool `
+    --version 5.*
+reportgenerator `
+    "-reports:$repoRoot/tests/Elzik.Breef.Api.Tests.Functional/TestResults/coverage.opencover.xml" `
+    "-targetdir:$repoRoot/tests/TestResults" `
+    "-reporttypes:Badges;Cobertura" `
+    "-filefilters:-*.g.cs" # https://github.com/danielpalme/ReportGenerator/issues/457
