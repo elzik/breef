ea91447068abb58aa108fcd2fb2eb39a25e27bd5
elzik <23397871+elzik@users.noreply.github.com>
2025-02-16 22:22:02 +0000

    Fix variable name typo

--- build/api/build-docker.ps1
+++ build/api/build-docker.ps1
@@ -8,7 +8,7 @@ Write-Output "Will be built as version: $Version"
 $repoRoot =  Resolve-Path "$PSScriptRoot/../.."
 
 docker build `
-    -t "ghcr.io/elzik/elzik-breef-api:$version" `
+    -t "ghcr.io/elzik/elzik-breef-api:$Version" `
         -f "$repoRoot/src/Elzik.Breef.Api/Dockerfile" `
     "$repoRoot/src"
 
