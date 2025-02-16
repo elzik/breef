27057cc43fd27d15535b68ab2f13cb7af70bac6b
elzik <23397871+elzik@users.noreply.github.com>
2025-02-16 10:49:06 +0000

    Output GitVersion result

--- build/api/get-semver.ps1
+++ build/api/get-semver.ps1
@@ -1,6 +1,7 @@
 dotnet tool install --global GitVersion.Tool --version 6.1.0
 
 $gitVersionOutput = dotnet-gitversion
-$gitVersionJson = $gitVersionOutput | ConvertFrom-Json
+Write-Output $gitVersionOutput
 
+$gitVersionJson = $gitVersionOutput | ConvertFrom-Json
 Write-Output $($gitVersionJson.SemVer)
