301ce68e2aa45d49b5aac22b066553fa4c074a82
elzik <23397871+elzik@users.noreply.github.com>
2025-02-16 21:53:26 +0000

    Set version numbers during assembky build

--- build/api/local-build.ps1
+++ build/api/local-build.ps1
@@ -1,6 +1,7 @@
-& $PSScriptRoot/restore.ps1
 $semver = & $PSScriptRoot/get-semver.ps1 | Select-Object -Last 1
 Write-Output "SemVer: $semver"
-& $PSScriptRoot/build.ps1
+
+& $PSScriptRoot/restore.ps1
+& $PSScriptRoot/build.ps1 -Version $semver
 & $PSScriptRoot/build-docker.ps1
 & $PSScriptRoot/test.ps1
