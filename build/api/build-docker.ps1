4b895504a590044e8d154bc82f2b84c8171aa3d2
elzik <23397871+elzik@users.noreply.github.com>
2025-05-10 22:31:08 +0100

    Publish API Docker Image (#31)
    * Move sem ver derivation to PowerShell script
    
    * Add GitVersion config
    
    * Set version numbers during assembky build
    
    * Remove unused secrets
    
    * Tag Docker image with semantic version
    
    * Surround variable in quotes
    
    * Fix errant quotes
    
    * Use correct syntax for writing to output
    
    * Pass version to Docker build
    
    * Fix variable name typo
    
    * Remove unecessary pwsh
    
    * Add debug output
    
    * Passs docker tag for tests
    
    * Add Docker tag & push steps
    
    * Add write oermisisons fro content  to release job
    
    * Fix Docker tagging
    
    * Pass Docker image between jobs as artifact
    
    * Upgrade artifact upload & download tasks
    
    * Add packages write permissions to release job
    
    * Only tag the main branch with latest
    
    * Improve step naming

--- build/api/build-docker.ps1
+++ build/api/build-docker.ps1
@@ -1,8 +1,15 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Version
+)
+
+Write-Output "Will be built as version: $Version"
+
 $repoRoot =  Resolve-Path "$PSScriptRoot/../.."
 
 docker build `
-    -t ghcr.io/elzik/elzik-breef-api:latest `
-        -f "$repoRoot/src/Elzik.Breef.Api/Dockerfile" `
+    -t "ghcr.io/elzik/elzik-breef-api:$Version" `
+    -f "$repoRoot/src/Elzik.Breef.Api/Dockerfile" `
     "$repoRoot/src"
 
 if ($LASTEXITCODE -ne 0) {
