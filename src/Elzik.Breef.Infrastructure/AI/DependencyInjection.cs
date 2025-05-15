2cb42a8d47d891e8e905b328a818424bec8393b5
elzik <23397871+elzik@users.noreply.github.com>
2025-05-15 17:49:58 +0100

    Avoid calling BuildServiceProvider in registration

--- src/Elzik.Breef.Infrastructure/AI/DependencyInjection.cs
+++ src/Elzik.Breef.Infrastructure/AI/DependencyInjection.cs
@@ -11,25 +11,31 @@ public static class DependencyInjection
 {
     public static void AddAiContentSummariser(this IServiceCollection services)
     {
-        var aiServiceOptions = services.BuildServiceProvider()
-                    .GetRequiredService<IOptions<AiServiceOptions>>().Value;
-
-        var kernelBuilder = aiServiceOptions.Provider switch
+        services.AddSingleton<Kernel>(sp =>
         {
-            AiServiceProviders.OpenAI =>
-                Kernel.CreateBuilder().AddOpenAIChatCompletion(aiServiceOptions.ModelId, new Uri(aiServiceOptions.EndpointUrl), aiServiceOptions.ApiKey),
-            AiServiceProviders.AzureOpenAI =>
-                Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(aiServiceOptions.ModelId, aiServiceOptions.EndpointUrl, aiServiceOptions.ApiKey),
-            AiServiceProviders.NotSet =>
-                throw new InvalidOperationException("AiService provider is not set."),
-            _ =>
-                throw new InvalidOperationException($"Unsupported AiService provider: {aiServiceOptions.Provider}"),
-        };
+            var aiServiceOptions = sp.GetRequiredService<IOptions<AiServiceOptions>>().Value;
+            var kernelBuilder = aiServiceOptions.Provider switch
+            {
+                AiServiceProviders.OpenAI =>
+                    Kernel.CreateBuilder().AddOpenAIChatCompletion(aiServiceOptions.ModelId, new Uri(aiServiceOptions.EndpointUrl), aiServiceOptions.ApiKey),
+                AiServiceProviders.AzureOpenAI =>
+                    Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(aiServiceOptions.ModelId, aiServiceOptions.EndpointUrl, aiServiceOptions.ApiKey),
+                AiServiceProviders.NotSet =>
+                    throw new InvalidOperationException("AiService provider is not set."),
+                _ =>
+                    throw new InvalidOperationException($"Unsupported AiService provider: {aiServiceOptions.Provider}"),
+            };
 
-        kernelBuilder.Services.AddSerilog();
-        var kernel = kernelBuilder.Build();
-        services.AddSingleton(kernel);
+            // Ideally, logging configuration should be centralized in the service layer.
+            // However, due to SemanticKernel’s separate DI container, this is currently required here for logging support.
+            // Without it SemanticKernel does not appear to log at all.
+            kernelBuilder.Services.AddSerilog();
+
+            return kernelBuilder.Build();
+        });
+
         services.AddScoped(sp => sp.GetRequiredService<Kernel>().GetRequiredService<IChatCompletionService>());
+
         services.AddTransient<IContentSummariser, AiContentSummariser>();
     }
 }
