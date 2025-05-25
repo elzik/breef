f4b36304ad66e3e8e6af1a0462f43264e8f39580
elzik <23397871+elzik@users.noreply.github.com>
2025-05-25 21:05:23 +0100

    Add partial SubRedditContentExtractor

---
+++ tests/Elzik.Breef.Infrastructure.Tests.Unit/ContentExtractors/SubRedditExtractorTests.cs
@@ -0,0 +1,55 @@
+﻿using Elzik.Breef.Domain;
+using Elzik.Breef.Infrastructure.ContentExtractors;
+using NSubstitute;
+using Shouldly;
+
+namespace Elzik.Breef.Infrastructure.Tests.Unit.ContentExtractors
+{
+    public class SubRedditExtractorTests
+    {
+        private readonly IWebPageDownloader _mockWebPageDownloader;
+
+        public SubRedditExtractorTests()
+        {
+            _mockWebPageDownloader = Substitute.For<IWebPageDownloader>();
+            _mockWebPageDownloader.DownloadAsync(Arg.Any<string>())
+                .Returns(Task.FromResult("<html><body>Mocked content</body></html>"));
+        }
+
+        [Theory]
+        [InlineData("https://reddit.com/r/testsubreddit/")]
+        [InlineData("https://reddit.com/r/testsubreddit")]
+        [InlineData("hTTpS://rEDdiT.cOm/R/tEsTsUbReDdIt/")]
+        [InlineData("https://www.reddit.com/r/testsubreddit/")]
+        public void CanHandle_ValidSubRedditUrl_ReturnsTrue(string url)
+        {
+            // Arrange
+            var extractor = new SubRedditContentExtractor(_mockWebPageDownloader);
+
+            // Act
+            var canHandle = extractor.CanHandle(url);
+
+            // Assert
+            canHandle.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData("not-a-url")]
+        [InlineData("https://reddit.com")]
+        [InlineData("https://reddit.com/r")]
+        [InlineData("https://reddit.com/r/testsubreddit/more")]
+        [InlineData("https://not-reddit.com/r/testsubreddit/")]
+        [InlineData("https://www2.reddit.com/r/testsubreddit/")]
+        public void CanHandle_InvalidSubRedditUrl_ReturnsFalse(string url)
+        {
+            // Arrange
+            var extractor = new SubRedditContentExtractor(_mockWebPageDownloader);
+
+            // Act
+            var canHandle = extractor.CanHandle(url);
+
+            // Assert
+            canHandle.ShouldBeFalse();
+        }
+    }
+}
