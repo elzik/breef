cb48db3cafe32acf79b8bc286bee5e57f085ce87
elzik <23397871+elzik@users.noreply.github.com>
2025-05-26 11:34:17 +0100

    Log strategy used

--- src/Elzik.Breef.Infrastructure/ContentExtractors/ContentExtractorStrategy.cs
+++ src/Elzik.Breef.Infrastructure/ContentExtractors/ContentExtractorStrategy.cs
@@ -1,15 +1,21 @@
 ﻿using Elzik.Breef.Domain;
+using Microsoft.Extensions.Logging;
 
 namespace Elzik.Breef.Infrastructure.ContentExtractors
 {
     public class ContentExtractorStrategy : IContentExtractor
     {
+        private readonly ILogger<ContentExtractorStrategy> _logger;
         private readonly List<IContentExtractor> _extractors;
 
-        public ContentExtractorStrategy(IEnumerable<IContentExtractor> specificExtractors, IContentExtractor defaultExtractor)
+        public ContentExtractorStrategy(ILogger<ContentExtractorStrategy> logger, 
+            IEnumerable<IContentExtractor> specificExtractors, IContentExtractor defaultExtractor)
         {
+            ArgumentNullException.ThrowIfNull(logger);
             ArgumentNullException.ThrowIfNull(specificExtractors);
             ArgumentNullException.ThrowIfNull(defaultExtractor);
+
+            _logger = logger;
 
             if (specificExtractors.Contains(defaultExtractor))
                 throw new ArgumentException("Default extractor should not be in the specific extractors list.");
@@ -22,6 +28,9 @@ namespace Elzik.Breef.Infrastructure.ContentExtractors
         public async Task<Extract> ExtractAsync(string webPageUrl)
         {
             var extractor = _extractors.First(e => e.CanHandle(webPageUrl));
+
+            _logger.LogInformation("Extraction will be provided for by {ExtractorName}", extractor.GetType().Name);
+
             return await extractor.ExtractAsync(webPageUrl);
         }
     }
