261b65ad318f87a19f565f9c4bf9155d8ffa8b43
elzik <23397871+elzik@users.noreply.github.com>
2025-05-26 22:17:56 +0100

    Add client for posts new in a Subreddit

---
+++ src/Elzik.Breef.Infrastructure/ContentExtractors/Reddit/Client/ISubredditClient.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+
+
+namespace Elzik.Breef.Infrastructure.ContentExtractors.Reddit.Client;
+
+public interface ISubredditClient
+{
+    [Get("/r/{subRedditName}/new.json")]
+    [Headers("User-Agent: breef/1.0.0 (https://github.com/elzik/breef)")]
+    Task<NewInSubreddit> GetNewInSubreddit(string subRedditName);
+}
