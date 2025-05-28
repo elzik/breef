85f45551ab6abba2c615369f54d28f3db809a879
elzik <23397871+elzik@users.noreply.github.com>
2025-05-28 22:21:13 +0100

    Initial reddit posts client

---
+++ src/Elzik.Breef.Infrastructure/ContentExtractors/Reddit/Client/IRedditPostClient.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elzik.Breef.Infrastructure.ContentExtractors.Reddit.Client
+{
+    public interface IRedditPostClient
+    {
+        [Get("/comments/{postId}.json")]
+        [Headers("User-Agent: breef/1.0.0 (https://github.com/elzik/breef)")]
+        Task<RedditPost> GetPost(string postId);
+    }
+}
