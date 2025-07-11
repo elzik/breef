dbd62d87b830a034204ffd67dceeb3f09797f4a6
elzik <23397871+elzik@users.noreply.github.com>
2025-07-11 23:32:23 +0100

    Initial tests for RedditRepliesConverter

--- src/Elzik.Breef.Infrastructure/ContentExtractors/Reddit/Client/RedditRepliesConverter.cs
+++ src/Elzik.Breef.Infrastructure/ContentExtractors/Reddit/Client/RedditRepliesConverter.cs
@@ -29,7 +29,11 @@ public class RedditRepliesConverter : JsonConverter<RedditListing>
             };
         }
 
-        var listing = JsonSerializer.Deserialize<RedditListing>(ref reader, options) 
+        // Create new options without this converter to prevent infinite recursion
+        var optionsWithoutThisConverter = new JsonSerializerOptions(options);
+        optionsWithoutThisConverter.Converters.Remove(optionsWithoutThisConverter.Converters.First(c => c is RedditRepliesConverter));
+
+        var listing = JsonSerializer.Deserialize<RedditListing>(ref reader, optionsWithoutThisConverter)
             ?? throw new InvalidOperationException("No Reddit listing was deserialized from the JSON.");
 
         listing.Data ??= new RedditListingData();
@@ -40,6 +44,10 @@ public class RedditRepliesConverter : JsonConverter<RedditListing>
 
     public override void Write(Utf8JsonWriter writer, RedditListing value, JsonSerializerOptions options)
     {
-        JsonSerializer.Serialize(writer, value, options);
+        // Create new options without this converter to prevent infinite recursion
+        var optionsWithoutThisConverter = new JsonSerializerOptions(options);
+        optionsWithoutThisConverter.Converters.Remove(optionsWithoutThisConverter.Converters.First(c => c is RedditRepliesConverter));
+
+        JsonSerializer.Serialize(writer, value, optionsWithoutThisConverter);
     }
 }
