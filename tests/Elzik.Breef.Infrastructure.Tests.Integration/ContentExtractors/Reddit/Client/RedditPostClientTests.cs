e0d777a7a860eee8a3c8a17f0486a9edac86eca6
elzik <23397871+elzik@users.noreply.github.com>
2025-09-20 22:34:17 +0100

    Make test less brittle and account for possible post deletion in the future

--- tests/Elzik.Breef.Infrastructure.Tests.Integration/ContentExtractors/Reddit/Client/RedditPostClientTests.cs
+++ tests/Elzik.Breef.Infrastructure.Tests.Integration/ContentExtractors/Reddit/Client/RedditPostClientTests.cs
@@ -38,22 +38,21 @@ public class RedditPostClientTests
             "engineer. This work ready college in Sweden has a 2 year long .net developer program with internships " +
             "at real companies. They also have a similar program but with javascript.\n\nI am wondering if this " +
             "would be a good path if my dream is to become a freelancer and I want to build easy apps / websites for " +
-            "small startups in Sweden/worldwide.\n\nThis is the program:\n\nProgramming C# ñ 12 weeks\n\nDevelopment " +
-            "against database and database administration ñ 9 weeks\n\nWeb development with .NET ñ 12 weeks\n\nAgile " +
-            "development ñ 6 weeks\n\nCustomer understanding, consulting and reporting ñ 3 weeks\n\nApprenticeship " +
-            "at companies ñ 12 weeks\n\nClean code ñ 6 weeks\n\nApprenticeship at companies ñ 16 weeks\n\nExam " +
-            "thesis ñ 4 weeks");
+            "small startups in Sweden/worldwide.\n\nThis is the program:\n\nProgramming C# ‚Äì 12 weeks\n\nDevelopment " +
+            "against database and database administration ‚Äì 9 weeks\n\nWeb development with .NET ‚Äì 12 weeks\n\nAgile " +
+            "development ‚Äì 6 weeks\n\nCustomer understanding, consulting and reporting ‚Äì 3 weeks\n\nApprenticeship " +
+            "at companies ‚Äì 12 weeks\n\nClean code ‚Äì 6 weeks\n\nApprenticeship at companies ‚Äì 16 weeks\n\nExam " +
+            "thesis ‚Äì 4 weeks");
         mainPost.Content.ShouldBe(mainPost.SelfText);
 
         var replies = redditPost[1].Data.Children;
 
         replies.Count.ShouldBe(5);
 
-        // First reply
-        replies[0].Kind.ShouldBe("t1");
-        replies[0].Data.Id.ShouldBe("mt7aaf6");
-        replies[0].Data.Author.ShouldBe("CodeRadDesign");
-        replies[0].Data.Body.ShouldBe(
+        var firstReply = replies.Single(r => r.Data.Id == "mt7aaf6");
+        firstReply.Kind.ShouldBe("t1");
+        firstReply.Data.Author.ShouldBeOneOf("CodeRadDesign", "[deleted]");
+        firstReply.Data.Body.ShouldBeOneOf(
             "not really.\n\nas someone who's been freelance on and off for 30 years, you're looking for a more " +
             "rounded skill set.  \n\nyou're not going to compete with 'people from third world countries' like the " +
             "other poster mentioned; you just can't. so you have to ask yourself, what do people in my area actually " +
@@ -67,54 +66,53 @@ public class RedditPostClientTests
             "and planning on getting a Real Job though haha.\n\n*regarding your last point on your other comment \"" +
             "c# looks easy to learn\" is not really a valid criteria. your first language is going to be the hardest" +
             ", your second language will be ten times easier. c# is a good foundational language tho, i'd recommend " +
-            "it over python because it teaches a lot of good habits early."
+            "it over python because it teaches a lot of good habits early.",
+            "[deleted]"
         );
 
-        // Second reply
-        replies[1].Kind.ShouldBe("t1");
-        replies[1].Data.Id.ShouldBe("mt7lqgx");
-        replies[1].Data.Author.ShouldBe("No_Researcher_7875");
-        replies[1].Data.Body.ShouldBe(
+        var secondReply = replies.Single(r => r.Data.Id == "mt7lqgx");
+        secondReply.Kind.ShouldBe("t1");
+        secondReply.Data.Author.ShouldBeOneOf("No_Researcher_7875", "[deleted]");
+        secondReply.Data.Body.ShouldBeOneOf(
             "As mentioned before it will be hard to compete with the experts but i think you are not thinking this " +
             "correctly.\n\n If you want to build sites, is not that important in wich language you code them but how " +
             "good and fast can you build them. \n\nThis program is a good start, and if you choose the js one would " +
             "be a little better mostly for the front end part.\n\nAnyways chose whatever program you like the most " +
-            "and code, code a lot and you will be able to do what you want."
+            "and code, code a lot and you will be able to do what you want.",
+            "[deleted]"
         );
 
-        // Third reply (has nested reply)
-        replies[2].Kind.ShouldBe("t1");
-        replies[2].Data.Id.ShouldBe("mt606l6");
-        replies[2].Data.Author.ShouldBe("[deleted]");
-        replies[2].Data.Body.ShouldBe("[deleted]");
+        var thirdReply = replies.Single(r => r.Data.Id == "mt606l6");
+        thirdReply.Kind.ShouldBe("t1");
+        thirdReply.Data.Author.ShouldBeOneOf("[deleted]");
+        thirdReply.Data.Body.ShouldBeOneOf("[deleted]");
 
-        // Fourth reply
-        replies[3].Kind.ShouldBe("t1");
-        replies[3].Data.Id.ShouldBe("mt83c0a");
-        replies[3].Data.Author.ShouldBe("goqsane");
-        replies[3].Data.Body.ShouldBe("No itís not.");
+        var fourthReply = replies.Single(r => r.Data.Id == "mt83c0a");
+        fourthReply.Kind.ShouldBe("t1");
+        fourthReply.Data.Author.ShouldBeOneOf("goqsane", "[deleted]");
+        fourthReply.Data.Body.ShouldBeOneOf("No it‚Äôs not.", "[deleted]");
 
-        // Fifth reply
-        replies[4].Kind.ShouldBe("t1");
-        replies[4].Data.Id.ShouldBe("mt9gc9x");
-        replies[4].Data.Author.ShouldBe("ToThePillory");
-        replies[4].Data.Body.ShouldBe(
+        var fifthReply = replies.Single(r => r.Data.Id == "mt9gc9x");
+        fifthReply.Kind.ShouldBe("t1");
+        fifthReply.Data.Author.ShouldBeOneOf("ToThePillory", "[deleted]");
+        fifthReply.Data.Body.ShouldBeOneOf(
             "I got most of my freelancing work in C#, that and Java.\n\nThe problem is that you're a beginner, and " +
             "freelancing doesn't really suit beginners, or even decent juniors.\n\nFreelancing means every single " +
             "problem you encounter is 100% your responsibility to fix. There is no team to bounce ideas off, there " +
             "is no manager to talk a client out of an idea, there is nobody other than you to solve \\*all\\* " +
             "problems.\n\nI would aim to get a regular programming job first, freelancing is not easy, and generally " +
-            "pays less than a normal job."
+            "pays less than a normal job.",
+            "[deleted]"
         );
 
-        // Nested reply to third reply
-        var nestedReplies = replies[2].Data.Replies.Data.Children;
+        var nestedReplies = thirdReply.Data.Replies.Data.Children;
         nestedReplies.Count.ShouldBe(1);
-        nestedReplies[0].Data.Id.ShouldBe("mt60jnv");
-        nestedReplies[0].Data.Author.ShouldBe("melvman1");
-        nestedReplies[0].Data.Body.ShouldBe(
+        var nestedReply = nestedReplies.Single(r => r.Data.Id == "mt60jnv");
+        nestedReply.Data.Author.ShouldBeOneOf("melvman1", "[deleted]");
+        nestedReply.Data.Body.ShouldBeOneOf(
             "I am willing to work at the company i do my apprenticeship at for a couple years to learn, but is this " +
-            "program a good start for my career if that is my îlong termî goal? :)"
+            "program a good start for my career if that is my ‚Äùlong term‚Äù goal? :)",
+            "[deleted]"
         );
     }
 }
