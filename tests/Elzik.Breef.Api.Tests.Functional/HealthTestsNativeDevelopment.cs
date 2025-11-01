040c833fe3c1fe631ff3c54e0af80868778852dc
elzik <23397871+elzik@users.noreply.github.com>
2025-11-01 19:10:15 +0000

    Use additional class to ensure tests cover exception handler as well as development excetion page

--- tests/Elzik.Breef.Api.Tests.Functional/HealthTestsNativeDevelopment.cs
+++ tests/Elzik.Breef.Api.Tests.Functional/HealthTestsNativeDevelopment.cs
@@ -2,15 +2,16 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Elzik.Breef.Api.Tests.Functional;
 
-public class HealthTestsNative : HealthTestsBase, IClassFixture<WebApplicationFactory<Program>>
+public class HealthTestsNativeDevelopment : HealthTestsBase, IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _webApplicationFactory;
     private readonly HttpClient _client;
 
     protected override HttpClient Client => _client;
 
-    public HealthTestsNative(WebApplicationFactory<Program> webAppFactory)
+    public HealthTestsNativeDevelopment(WebApplicationFactory<Program> webAppFactory)
     {
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
         _webApplicationFactory = webAppFactory;
         _client = _webApplicationFactory.CreateClient();
     }
