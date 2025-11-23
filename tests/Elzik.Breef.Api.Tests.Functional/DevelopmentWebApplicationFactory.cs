89c82cfeb304d17132b50d5c4b11468839a6fbbe
elzik <23397871+elzik@users.noreply.github.com>
2025-11-23 22:32:06 +0000

    Refactor deprecated ConfigureWebHost override pattern

--- tests/Elzik.Breef.Api.Tests.Functional/DevelopmentWebApplicationFactory.cs
+++ tests/Elzik.Breef.Api.Tests.Functional/DevelopmentWebApplicationFactory.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
 
 namespace Elzik.Breef.Api.Tests.Functional;
 
 public class DevelopmentWebApplicationFactory : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    protected override IHost CreateHost(IHostBuilder builder)
     {
         builder.UseEnvironment("Development");
+        return base.CreateHost(builder);
     }
 }
